ded38f083eaa6be260733688fe80a863b3bf599e
= <paradox.thunderfury@gmail.com>
2024-11-21 12:35:20 -0700

    Update image sizing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,6 +10,6 @@
   <main>
     <p class="text-5xl text-center mt-10">Coming Soon...</p>
 
-    <img class="mx-auto mt-16 animate-spin-slow" src="/src/assets/images/freshadox-transparent.png">
+    <img class="mx-auto mt-16 animate-spin-slow size-96" src="/src/assets/images/freshadox-transparent.png">
   </main>
 </template>
